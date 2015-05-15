682483b8a4dd1a21b8534781c617851d70ac5887
Jeff Cross <middlefloor@gmail.com>
2015-05-15 15:53:40 -0700

    feat(AuthService): create auth service to manage global auth state

--- src/components/auth-service/auth-service.ts
+++ src/components/auth-service/auth-service.ts
@@ -1,17 +1,30 @@
-/// <reference path="../../../typings/rx/rx.lite.d.ts" />
+/// <reference path="../../../typings/rx/rx.all.d.ts" />
 /// <reference path="../../../typings/firebase/firebase.d.ts" />
 declare var Rx;
 import {FIREBASE_PATH} from '../../config';
+import {EventEmitter} from "angular2/angular2";
 
 export class AuthService {
-  authObservable: Rx.Observable<FirebaseAuthResult>;
+  firebaseRef: Firebase;
+  authObservable: EventEmitter;
   constructor() {
-    var firebaseRef = new Firebase(FIREBASE_PATH);
-    var authResult = firebaseRef.getAuth();
-    this.authObservable = Rx.Observable.create((observer: Rx.Observer<FirebaseAuthResult>) => {
-      firebaseRef.onAuth(onAuth);
-      function onAuth(result: FirebaseAuthResult) { observer.onNext(result); }
-      return function() { firebaseRef.offAuth(onAuth); }
+    this.firebaseRef = new Firebase(FIREBASE_PATH);
+    this.authObservable = new EventEmitter();
+    // TODO: don't use EventEmitter, just create a pipe
+    this.authObservable._subject = new Rx.ReplaySubject();
+    this.firebaseRef.onAuth(
+        (result: FirebaseAuthResult) => { this.authObservable._subject.onNext(result); });
+  }
+
+  login(): void {
+    this.firebaseRef.authWithOAuthPopup('github', e => {
+      if (e) return this.authObservable._subject.onError(e);
+      this.authObservable._subject.onNext(this.firebaseRef.getAuth());
     });
+  }
+
+  logout(): void {
+    this.firebaseRef.unauth();
+    this.authObservable._subject.onNext(null);
   }
 }
